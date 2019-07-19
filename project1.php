534353fa7d943e73486c2b9459422ec6567240bc
shivtomar51 <53076186+shivtomar51@users.noreply.github.com>
2019-07-19 17:47:38 +0530

    Add files via upload

---
+++ project1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JEC Login</title>
+	<link rel="stylesheet" type="text/css" href="loginstyle.css">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<style>
+			body{
+	               background-image: url("235624ffcf6f868.JPG");background-repeat: no-repeat; width: 100%
+	 			   
+    }
+	</style>
+	
+
+
+</head>
+<body>
+	<div class="row">
+		<h1 id="header">JABALPUR ENGINEERING COLLEGE FORUM</h1>
+	</div> 
+	<div class="row">
+		<div class="col-lg-3 col-md-1 col-sm-1"></div>
+		<div class="col-lg-6 col-md-10 col-sm-10">
+			<div id="loginbox">
+			<form id="formstyle"> 
+				<b>USERNAME:</b><br>
+				 <input class="logininputstyle" type="text" name="username" autofocus="" required=""><br><br>
+				<b>PASSWORD:</b><br>
+				 <input class="logininputstyle" type="password" name="password" required=""><br><br>
+				 <button type="button" name="submit" value="submit">Log in</button>
+			</form><br><br>
+              	   <p>Don't have account.<button type="button">register here</button></p>
+			</div>
+		</div>
+		<div class="col-lg-3 col-md-1 col-sm-1"></div>
+	</div>
+</body>
+</html>
